0976c561a75cf67b17b69910d3f8dc7af124b1a3
huriferenc <huri.ferenc@gmail.com>
2023-02-10 11:34:18 +0100

    Removed auto-unsubscriber

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,50 +1,67 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { filter, Subject, takeUntil } from 'rxjs';
 
-import AutoUnsubscribe from './auto-unsubscribe.decorator';
 import { IntersectionObserverService } from './intersection-observer.service';
 
-@AutoUnsubscribe()
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('intro', { read: ElementRef }) intro: ElementRef;
   @ViewChild('aboutMe', { read: ElementRef }) aboutMe: ElementRef;
   @ViewChild('skills', { read: ElementRef }) skills: ElementRef;
   @ViewChild('references', { read: ElementRef }) references: ElementRef;
+
+  private onDestroy$ = new Subject<void>();
 
   constructor(private intersectionObserver: IntersectionObserverService) {}
 
   ngAfterViewInit() {
     this.intersectionObserver
       .createAndObserve(this.intro)
-      .pipe(filter((isVisible: boolean) => isVisible))
+      .pipe(
+        takeUntil(this.onDestroy$),
+        filter((isVisible: boolean) => isVisible)
+      )
       .subscribe(() => {
         this.intro.nativeElement.classList.add('visible');
       });
 
     this.intersectionObserver
       .createAndObserve(this.aboutMe)
-      .pipe(filter((isVisible: boolean) => isVisible))
+      .pipe(
+        takeUntil(this.onDestroy$),
+        filter((isVisible: boolean) => isVisible)
+      )
       .subscribe(() => {
         this.aboutMe.nativeElement.classList.add('visible');
       });
 
     this.intersectionObserver
       .createAndObserve(this.skills)
-      .pipe(filter((isVisible: boolean) => isVisible))
+      .pipe(
+        takeUntil(this.onDestroy$),
+        filter((isVisible: boolean) => isVisible)
+      )
       .subscribe(() => {
         this.skills.nativeElement.classList.add('visible');
       });
 
     this.intersectionObserver
       .createAndObserve(this.references)
-      .pipe(filter((isVisible: boolean) => isVisible))
+      .pipe(
+        takeUntil(this.onDestroy$),
+        filter((isVisible: boolean) => isVisible)
+      )
       .subscribe(() => {
         this.references.nativeElement.classList.add('visible');
       });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 }
